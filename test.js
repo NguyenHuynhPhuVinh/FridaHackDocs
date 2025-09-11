10dff9d1ce45fe4a3006d6dbe33f974508f12308
TomiSakae <tomisakaeap3@gmail.com>
2025-09-11 14:37:32 +0700

    Remove unused files: HHPT-Bypassed.apk, smart_apk_puller.py, test.js, and apktool_2.9.3.jar

--- test.js
+++
@@ -1,5 +0,0 @@
-console.log("[+] Frida script injected successfully!");
-console.log("[*] Target application is running and hooked.");
-console.log(
-  "[!] You can now stop this script (Ctrl + C) and run your main s1.js script."
-);
