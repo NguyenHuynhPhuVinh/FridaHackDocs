925c732756fccf890eef7ba8c0d059cb79b66efb
TomiSakae <tomisakaeap3@gmail.com>
2025-09-13 17:34:59 +0700

    Refactor damage handling and enhance God Mode functionality in Main.cpp

--- com.gamepub.pt/Main.cpp
+++ com.gamepub.pt/Main.cpp
@@ -1,3 +1,5 @@
+// --- START OF FILE Main.cpp ---
+
 #include <list>
 #include <vector>
 #include <cstring>
@@ -14,9 +16,8 @@
 #include "Menu/Menu.hpp"
 #include "Menu/Jni.hpp"
 #include "Includes/Macros.h"
-#include "KittyMemory/KittyInclude.hpp" // THÊM DÒNG NÀY
+#include "KittyMemory/KittyInclude.hpp"
 
-// Dobby/Substrate/And64InlineHook
 #if defined(__aarch64__)
 #include "And64InlineHook/And64InlineHook.hpp"
 #else
@@ -28,23 +29,16 @@
 
 // ===============================================================================================
 // === MOD MENU CHO GAME "CHUYỆN CÔNG CHÚA" (com.gamepub.pt) ===
-// Dựa trên phân tích từ Frida
 // ===============================================================================================
 
-// === ĐỊNH NGHĨA CÁC STRUCT C# TRONG C++ ===
+namespace GameOffsets {
+    const uintptr_t AddHP = 0xd7e10c;
+    const uintptr_t get_IsPlayer = 0xd71e70;
+}
 
-struct Battle_StatTransfer_o {
-    char padding[0x1c];
-    float Damage;
-};
-
-struct Battle_DamageResult_o {
-    char data[8];
-};
-
+// === KHAI BÁO BIẾN TOÀN CỤC CHO CÁC CHỨC NĂNG ===
 bool isGodModeEnabled = false;
-bool isOneHitKillEnabled = false;
-const float DAMAGE_MULTIPLIER = 1000.0f;
+int damageMultiplier = 1; // Giá trị mặc định là 1 (không thay đổi sát thương)
 
 bool (*isPlayer_ptr)(void *instance);
 
@@ -55,54 +49,52 @@ bool isPlayer(void *characterBaseInstance) {
     return false;
 }
 
-// === KHAI BÁO HÀM GỐC VÀ HÀM GIẢ (HOOK) ===
-Battle_DamageResult_o (*orig_RecieveDamage)(void *instance, void *attacker, void *statTransfer, bool bAddDamage, void *vAttackPOS);
-Battle_DamageResult_o fake_RecieveDamage(void *instance, void *attacker, void *statTransfer, bool bAddDamage, void *vAttackPOS) {
-    bool receiverIsPlayer = isPlayer(instance);
-    bool attackerIsPlayer = isPlayer(attacker);
+// === HOOK HÀM ADDHP ===
+void (*orig_AddHP)(void* instance, float addValue, void* caster, bool isAddDamage, int markType);
 
-    Battle_StatTransfer_o *pStat = (Battle_StatTransfer_o *)statTransfer;
-    float originalDamage = pStat->Damage;
+void fake_AddHP(void* instance, float addValue, void* caster, bool isAddDamage, int markType) {
+    if (instance == nullptr || caster == nullptr) {
+        return orig_AddHP(instance, addValue, caster, isAddDamage, markType);
+    }
 
-    if (isGodModeEnabled && receiverIsPlayer && !attackerIsPlayer) {
-        if (originalDamage > 1.0f) {
-            LOGD(OBFUSCATE("[God Mode] Blocked damage: %.0f -> 1"), originalDamage);
-            pStat->Damage = 1.0f;
+    if (addValue < 0) {
+        bool receiverIsPlayer = isPlayer(instance);
+        bool casterIsPlayer = isPlayer(caster);
+
+        if (isGodModeEnabled && receiverIsPlayer && !casterIsPlayer) {
+            LOGD(OBFUSCATE("[God Mode] Blocked HP loss of %.0f"), addValue);
+            return;
+        }
+
+        if (damageMultiplier > 1 && !receiverIsPlayer && casterIsPlayer) {
+            float originalDamage = addValue;
+            float multipliedDamage = originalDamage * damageMultiplier;
+            LOGD(OBFUSCATE("[Damage x%d] Overriding HP loss from %.0f to %.0f"), damageMultiplier, originalDamage, multipliedDamage);
+            return orig_AddHP(instance, multipliedDamage, caster, isAddDamage, markType);
         }
     }
 
-    if (isOneHitKillEnabled && !receiverIsPlayer && attackerIsPlayer) {
-        if (originalDamage > 0.0f) {
-            float newDamage = originalDamage * DAMAGE_MULTIPLIER;
-            LOGD(OBFUSCATE("[One-Hit] Modified damage: %.0f -> %.0f"), originalDamage, newDamage);
-            pStat->Damage = newDamage;
-        }
-    }
-
-    return orig_RecieveDamage(instance, attacker, statTransfer, bAddDamage, vAttackPOS);
+    return orig_AddHP(instance, addValue, caster, isAddDamage, markType);
 }
 
+
 // ===============================================================================================
-// === TÍCH HỢP VÀO MENU ===
+// === TÍCH HỢP VÀO MENU (ĐÃ CẬP NHẬT) ===
 // ===============================================================================================
 
 jobjectArray GetFeatureList(JNIEnv *env, jobject context) {
     jobjectArray ret;
-
     const char *features[] = {
             OBFUSCATE("Category_Mod Chuyện Công Chúa by TomiSakae!"),
             OBFUSCATE("0_Toggle_God Mode (Bất Tử)"),
-            OBFUSCATE("1_Toggle_One-Hit Kill (Sát thương x1000)"),
+            // Thêm InputValue và dùng chung feature number là 1
+            OBFUSCATE("1_SeekBar_Hệ Số Sát Thương_1_1000"),
+            OBFUSCATE("1_InputValue_Nhập Hệ Số Sát Thương"),
     };
-
     int Total_Feature = (sizeof features / sizeof features[0]);
-    ret = (jobjectArray)
-            env->NewObjectArray(Total_Feature, env->FindClass(OBFUSCATE("java/lang/String")),
-                                env->NewStringUTF(""));
-
+    ret = (jobjectArray)env->NewObjectArray(Total_Feature, env->FindClass(OBFUSCATE("java/lang/String")), env->NewStringUTF(""));
     for (int i = 0; i < Total_Feature; i++)
         env->SetObjectArrayElement(ret, i, env->NewStringUTF(features[i]));
-
     return (ret);
 }
 
@@ -110,36 +102,54 @@ void Changes(JNIEnv *env, jclass clazz, jobject obj, jint featNum, jstring featN
     switch (featNum) {
         case 0:
             isGodModeEnabled = boolean;
+            LOGI("[+] God Mode %s", isGodModeEnabled ? "ENABLED" : "DISABLED");
             break;
         case 1:
-            isOneHitKillEnabled = boolean;
+            // Cả SeekBar và InputValue đều gọi đến case này
+            // Giá trị `value` sẽ được cập nhật bởi control nào được người dùng tương tác cuối cùng
+            if (value >= 1) {
+                damageMultiplier = value;
+            } else {
+                damageMultiplier = 1; // Nếu nhập số 0 hoặc số âm, coi như tắt
+            }
+
+            if (damageMultiplier > 1) {
+                LOGI("[+] Damage Multiplier set to x%d", damageMultiplier);
+            } else {
+                LOGI("[+] Damage Multiplier DISABLED");
+            }
             break;
     }
 }
 
+// === HACK THREAD (KHÔNG THAY ĐỔI) ===
 void hack_thread() {
     LOGI(OBFUSCATE("Hack thread started. Waiting for %s"), (const char *) targetLibName);
 
     do {
         sleep(1);
     } while (!isLibraryLoaded(targetLibName));
+    LOGI(OBFUSCATE("%s is now loaded. Trying to get base address..."), (const char *) targetLibName);
 
-    // SỬA LỖI Ở ĐÂY: Dùng KittyMemory để lấy địa chỉ cơ sở
-    KittyMemory::ProcMap il2cppMap = KittyMemory::getElfBaseMap(targetLibName);
-    while (!il2cppMap.isValid()) {
-        LOGD(OBFUSCATE("Waiting for %s to be fully loaded in memory..."), (const char *) targetLibName);
-        il2cppMap = KittyMemory::getElfBaseMap(targetLibName);
-        sleep(1);
-    }
-    uintptr_t il2cpp_base = il2cppMap.startAddress;
+    uintptr_t il2cpp_base = 0;
+    do {
+        il2cpp_base = findLibrary(targetLibName);
+        if (il2cpp_base == 0) {
+            LOGD("Still waiting for %s base address...", (const char *) targetLibName);
+            sleep(1);
+        }
+    } while (il2cpp_base == 0);
 
     LOGI(OBFUSCATE("%s has been loaded at %p. Applying hooks..."), (const char *) targetLibName, (void*)il2cpp_base);
 
 #if defined(__aarch64__)
-    isPlayer_ptr = (bool (*)(void*)) (il2cpp_base + 0xd71e70);
+    isPlayer_ptr = (bool (*)(void*)) (il2cpp_base + GameOffsets::get_IsPlayer);
     LOGI(OBFUSCATE("get_IsPlayer function located at: %p"), isPlayer_ptr);
 
-    HOOK(targetLibName, 0xd7f560, fake_RecieveDamage, orig_RecieveDamage);
+    uintptr_t addHPAddr = il2cpp_base + GameOffsets::AddHP;
+    LOGI(OBFUSCATE("Hooking AddHP at: %p"), (void*)addHPAddr);
+    A64HookFunction((void *)addHPAddr, (void *)fake_AddHP, (void **)&orig_AddHP);
+
 #elif defined(__arm__)
     // Thêm offset cho ARMv7 nếu cần
 #endif
@@ -151,3 +161,5 @@ __attribute__((constructor))
 void lib_main() {
     std::thread(hack_thread).detach();
 }
+
+// --- END OF FILE Main.cpp ---
