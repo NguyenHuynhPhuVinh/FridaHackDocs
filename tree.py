35da4db3b4cae9285dfa7a04efe8734f98d0c18c
TomiSakae <tomisakaeap3@gmail.com>
2025-09-13 22:23:42 +0700

    Update export_tree function call to use a new directory path for output

--- tree.py
+++ tree.py
@@ -11,4 +11,4 @@ def export_tree(startpath, output_file):
                 f.write(f'{subindent}{file}\n')
 
 # Sử dụng với thư mục exize.tapas.girlAdrift
-export_tree('exize.tapas.girlAdrift', 'directory_tree_with_files.txt')
+export_tree(r'C:\Users\tomis\Documents\APK Easy Tool v1.60 Portable\1-Decompiled APKs\ccc-bypass', 'directory_tree_with_files.txt')
